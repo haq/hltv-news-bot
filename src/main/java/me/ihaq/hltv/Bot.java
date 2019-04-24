0f5eac6d41e07432b3d7d446b951f4d022b572fa
haq <haqgamer66@gmail.com>
2019-04-23 22:52:38 -0400

    cleaned up

--- src/main/java/me/ihaq/hltv/Bot.java
+++ src/main/java/me/ihaq/hltv/Bot.java
@@ -1,20 +1,35 @@
 package me.ihaq.hltv;
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
-    public static class Data {
+    private List<Integer> users = new ArrayList<Integer>();
+
+    public static final class Data {
         public static final String NAME = "HLTV News Bot";
         public static final String TOKEN = "";
         public static final String RSS_LINK = "https://www.hltv.org/rss/news";
+
+        public static final class Commands {
+            public static final String START = "/start";
+            public static final String STOP = "/stop";
+        }
     }
 
     public static void main(String[] args) {
@@ -34,16 +49,31 @@ public class Bot extends TelegramLongPollingBot {
             String text = message.getText();
             long chatId = message.getChatId();
 
-            try {
-                execute(
-                        new SendMessage()
-                                .setChatId(chatId)
-                                .setText("test")
-                                .enableMarkdown(true)
-                );
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if (text.equals(Data.Commands.START)) {
+                // add user to list
+            } else if (text.equals(Data.Commands.STOP)) {
+                // remover user from the list
             }
+        }
+    }
+
+    public void readNews() {
+        try {
+            SyndFeed feed = new SyndFeedInput().build(
+                    new XmlReader(new URL(Data.RSS_LINK))
+            );
+
+            for (SyndEntry entry : feed.getEntries()) {
+                System.out.println(entry.getTitle());
+                System.out.println(entry.getDescription().getValue());
+                System.out.println(entry.getLink());
+                System.out.println(entry.getPublishedDate());
+                System.out.println();
+            }
+        } catch (FeedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
